f18262587febb6cd6b25f9cefd34461ab0574b67
Elter <64990477+Maple-Elter@users.noreply.github.com>
2021-07-06 22:11:42 -0600

    Added some features

--- ytalbumdl.py
+++ ytalbumdl.py
@@ -13,10 +13,9 @@ def do_stuff(video_count, list1):
 			print("Downloading Song", current_index, "of", video_count, "		", downloaded_percent, "%", end='\r')
 
 
-
 	#options for youtube_dl
 	yld_opts = {
-		'outtmpl': '%(track)s.%(ext)s',
+		'outtmpl': '%(title)s.%(ext)s',
 		'quiet':True,
 		'format' : 'bestaudio/best',
 		'postprocessors' : [{
@@ -38,39 +37,57 @@ def do_stuff(video_count, list1):
 
 
 
+if __name__ == "__main__":
 
-#main function
-def main():
+	#main function
+	def main():
+
+		# 1 means Playlist while 2 means individual video
+		url_type = 0
+
+		#gets URL of playlist or video from user
+		video_url = input("Enter video or playlist URL of song: ")
+
+		#checks if url is a valid youtube playlist
+		if not video_url or "youtube.com" not in video_url:
+			print("Enter a valid youtube URL")
 
 
-	#gets URL of playlist from user
-	video_playlist = input("Enter an album URL: ")
+		#if url is video in playlist rather than playlist url itself, this converts it
+		if 'list' in video_url:
+			new_list = video_url.split('&')
+			video_url = "https://www.youtube.com/playlist?" + new_list[1]
 
-	#checks if url is a valid youtube playlist
-	if not video_playlist or "youtube.com" not in video_playlist or 'list' not in video_playlist:
-		print("Enter a yotube playlist video URL")
+		#sets mode
+		if 'playlist' in video_url:
+			url_type = 1
+		elif 'watch' in video_url:
+			url_type = 2
 
-	#if url is video in playlist rather than playlist url itself, this converts it
-	if 'watch' in video_playlist:
-		new_list = video_playlist.split('&')
-		video_playlist = "https://www.youtube.com/playlist?" + new_list[1]
-
-	#if is proper playlist url
-	if 'playlist' in video_playlist:
-
+		#list of videos
 		list1 = []
-		#retrieves video count of playlist as int and saves it to video_count
-		playlist = Playlist.getInfo(video_playlist)
-		video_count = int(playlist['videoCount'])
+		#defaults to 1 for single video
+		video_count = 1
 
-		#gets urls of videos in playlist and saves to list
-		video = Playlist.getVideos(video_playlist)
-		for i in range(video_count):
-			try:
-				list1.append(video['videos'][i]['link'])
-			except IndexError:
-				break
+		#if playlist
+		if url_type == 1:
+			#retrieves video count of playlist as int and saves it to video_count
+			playlist = Playlist.getInfo(video_url)
+			video_count = int(playlist['videoCount'])
 
+			#gets urls of videos in playlist and saves to list
+			videos = Playlist.getVideos(video_url)
+			for i in range(video_count):
+				try:
+					list1.append(videos['videos'][i]['link'])
+				except IndexError:
+					break
+
+		#if single video
+		if url_type == 2:
+			list1.append(video_url)
+
+		#downloads the video(s)
 		do_stuff(video_count, list1)
 
 
